d365151cb4241056d263123d5cdbb9e4ec3a3448
Eplepai96 <bjorn.andreas1996@gmail.com>
2022-12-26 17:56:28 +0100

    Just added semicolons

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const postsUrl = "https://www.eplepaisolutions.com/wp-json/wp/v2/posts?page=1&_e
 async function getPosts(url) {
     const response = await fetch(url);
     const posts = await response.json();
-    console.log(response)
+    console.log(response);
 
     posts.forEach(function(post){
         container.innerHTML += `<h2>${post.title.rendered}</h2>
                                 <div>${post.content.rendered}</div>
                                 <img style="max-width: 100%" src="${post._embedded?.["wp:featuredmedia"]?.[0].source_url}"/>`
-    })
+    });
     
 }
 
-getPosts(postsUrl)
+getPosts(postsUrl);
